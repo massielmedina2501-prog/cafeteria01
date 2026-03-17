ccacd0357dc960e8d9e81ccd86cc3471d2b83b4e
massielmedina2501-prog <massielmedina2501@alumnos.ubiobio.cl>
2026-03-17 18:37:00 -0300

    agregando clases

--- src/Cafe.java
+++ src/Cafe.java
@@ -1,4 +1,18 @@
-package PACKAGE_NAME;
-
 public class Cafe {
+    private String nombre;
+    private String tamano;
+    private Double precio;
+
+    public Cafe(String nombre, String tamaño, Double precio){
+        this.nombre= nombre;
+        this.tamano= tamaño;
+        this.precio= precio;
+    }
+
+    public void mostrarticket(){
+        System.out.println("==== TICKET ====");
+        System.out.println("Café: " + nombre);
+        System.out.println("Tamaño: " + tamano);
+        System.out.println("Precio: $" + precio);
+    }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,20 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
-    public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+import java.util.Scanner;
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+public class Main{
+    public static void main (String[] args){
+        Scanner sc= new Scanner(System.in);
+
+        System.out.println("Bienvenido");
+        System.out.println("ingrese nombre del cafe:");
+        String nombre = sc.nextLine();
+        System.out.println("ingrese tamaño del cafe:");
+        String tamano = sc.nextLine();
+        System.out.println("ingrese precio del cafe:");
+        double precio = sc.nextDouble();
+
+        Cafe cafe1= new Cafe(nombre, tamano, precio);
+        cafe1.mostrarticket();
+
     }
+
 }
